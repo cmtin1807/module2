2b22fe4c7da32045d4d4c0d5f030ffc3f897371a
cmtin1807 <cmtin1807@gmail.com>
2024-06-12 13:57:21 +0700

    Hello19

--- src/ss4/bai_tap/PhuongTrinhBac2/MainPhuongTrinhBac2.java
+++ src/ss4/bai_tap/PhuongTrinhBac2/MainPhuongTrinhBac2.java
@@ -15,31 +15,3 @@ public class MainPhuongTrinhBac2 {
         System.out.println(phuongTrinhBac2.nghiem());
     }
 }
-class PhuongTrinhBac2 {
-        double a, b, c;
-
-        public PhuongTrinhBac2(double a, double b, double c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-
-        public double delta() {
-            return b * b - 4 * a * c;
-        }
-
-        public String nghiem() {
-            double d = delta();
-            if (d > 0) {
-                double x1 = (-b + Math.sqrt(d)) / (2 * a);
-                double x2 = (-b - Math.sqrt(d)) / (2 * a);
-                return "Phương trình có 2 nghiệm: x1 = " + x1 + ", x2 = " + x2;
-            } else if (d == 0) {
-                double x = -b / (2 * a);
-                return "Phương trình có nghiệm kép: x = " + x;
-            } else {
-                return "Phương trình vô nghiệm";
-            }
-        }
-    }
-
