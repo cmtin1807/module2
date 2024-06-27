ec668b4a30289f607822e6b668e9d0e6a67f7aa1
cmtin1807 <cmtin1807@gmail.com>
2024-06-27 09:13:11 +0700

    Hello50

--- src/ss16/thuc_hanh_test/test_file_class/Manager/Main.java
+++ src/ss16/thuc_hanh_test/test_file_class/Manager/Main.java
@@ -2,7 +2,9 @@ package ss16.thuc_hanh_test.test_file_class.Manager;
 
 
 import java.io.File;
-import java.io.PrintWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +13,25 @@ public class Main {
 
         try{
             File file = new File("src//ss16//thuc_hanh_test//test_file_class//Manager//Hellodadcac.txt");
-            System.out.println(file.exists());
-
+            try{
+                if (!file.exists()){
+                    throw new FileNotFoundException("File not found....");
+                }
+            }
+            catch (FileNotFoundException e){
+                System.err.println(e.getMessage());
+            }
+            FileWriter fw = new FileWriter(file,true);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter fName: ");
+            String fName = sc.nextLine();
+            System.out.println("Enter lName: ");
+            String lName = sc.nextLine();
+            System.out.println("Enter age: ");
+            int age1 = sc.nextInt();
+            String data = "\n"+  fName + " " + lName + " " + age1;
+            fw.write(data);
+            fw.close();
             Scanner scanner = new Scanner(file);
             while(scanner.hasNextLine()){
                 String fistName = scanner.next();
